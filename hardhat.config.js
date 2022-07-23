7af483c560ccaea2cc4b9fb9ba639b23adb0096c
Davit Mkhitaryan <davitmkhitaryan@Davits-MacBook-Pro.local>
2022-07-23 19:18:56 +0400

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("./scripts/deploy.js");
+require("./scripts/mint.js");
+require("@nomiclabs/hardhat-etherscan");
+
+const { ALCHEMY_KEY_GOERLI, ALCHEMY_KEY_RINKEBY, ALCHEMY_KEY_POLYGON, ALCHEMY_KEY_MAINNET, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.1",
+   defaultNetwork: "goerli",
+   networks: {
+    hardhat: {},
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY_RINKEBY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY_MAINNET}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    polygon: {
+      chainId: 80001,
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY_POLYGON}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    goerli: {
+      chainId: 5,
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_KEY_GOERLI}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: "K5UKS9SF2W4HAB65F9MSH59ZY9QV6F63GU"
+  }
+}
